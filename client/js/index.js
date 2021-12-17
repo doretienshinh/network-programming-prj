226ea275bef44877a8d276aba5eab33859fa8faa
Duy Linh <doretienshinh2@gmail.com>
2021-12-17 11:36:43 +0700

    11%

--- client/js/index.js
+++ client/js/index.js
@@ -1,19 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
-    enterGameState();
-});
-var enterGame = false;
-function enterGameState() {
-    if (enterGame) {
-        $('#game-screen').show('slow');
-        $('#menu').hide();
-    }
-    else {
-        $('#game-screen').hide();
-        $('#menu').show('slow');
-    }
-}
 
-function playGame() {
-    enterGame = !enterGame;
-    enterGameState();
+    var enterGame = false;
+
+    $.fn.enterGameState = function () {
+        if (enterGame) {
+            $('#game-screen').show('slow');
+            $('.runner-canvas').addClass('show-canvas');
+            $('#menu').hide('slow');
+        }
+        else {
+            $('#game-screen').hide('slow');
+            $('.runner-canvas').removeClass('show-canvas');
+            $('#menu').show('slow');
+        }
+    }
+
+    $.fn.enterGameState();
+
+    $('#playGame').click(function () {
+        enterGame = !enterGame;
+        $.fn.enterGameState();
+    })
+    $('.back-menu').click(function () {
+        enterGame = !enterGame;
+        $.fn.enterGameState();
+    })
+});
+
+function logout(){
+    location.href = './login.html';
 }
